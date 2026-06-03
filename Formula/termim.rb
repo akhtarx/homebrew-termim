d14ab88a314bccf276865b0beb7c4c0331af50ab
Md Mim Akhtar <mdmimakhtar@gmail.com>
2026-06-03 17:14:02 +0530

    chore: update formula to v1.1.5

--- Formula/termim.rb
+++ Formula/termim.rb
@@ -4,7 +4,7 @@ class Termim < Formula
   url "https://github.com/akhtarx/termim/archive/refs/tags/v1.1.5.tar.gz"
   # Note: When releasing v1.1.5, update this SHA256 checksum with the value of the release tarball.
   # You can find it by running: curl -sSL https://github.com/akhtarx/termim/archive/refs/tags/v1.1.5.tar.gz | sha256sum
-  sha256 "a47a35aec4c50acb8c30863cc34cd31c6293fcc12c05aacb37a8d801410131ac"
+  sha256 "0cf7b3e925c2748f5a3f75dccbb74dc3f13d2b34a2ed8a18818f8101942d9185"
   license "MIT"
 
   depends_on "rust" => :build
